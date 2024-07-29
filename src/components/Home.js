2fbe3d525d7e2d315290dbe6bd6216dd30be6663
ISLAVATH SATHISH NAIK <53205415+Sathishnaik786@users.noreply.github.com>
2024-07-29 18:29:57 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,7 +11,7 @@ const Home = () => {
   return (
     <div className="home">
       <header className="home-header">
-        <h2>Welcome to the MeeCodex Solutions</h2>
+        <h1><b>Welcome to MeeCodex Solutions</b></h1>
       </header>
 
       <div className="home-content">
